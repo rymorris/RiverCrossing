adcda5db1172ceb348489087fcc10e4dafb9b56c
Ryan <rymorris1@gmail.com>
2019-04-23 12:46:31 -0700

    nomination ajax google sheet initial

---
+++ js/nominate_ajax.js
@@ -0,0 +1,17 @@
+
+
+
+var $form = $('form#nominate'),
+    url = 'https://script.google.com/macros/s/AKfycbx3AWI0XLyJpeG-AtUXnyIfXyvFAhbL99ajCuM1iMJSm3zOBwc/exec'
+
+$('#nominationSubmit').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
