90abe4ebb49712bb7dc44ba928f188992a49a38f
Ryan <rymorris1@gmail.com>
2019-04-24 19:29:55 -0700

    contact form to google submission

--- js/contact_ajax.js
+++ js/contact_ajax.js
@@ -11,9 +11,27 @@ $('#contactSubmit').on('click', function(e) {
     method: "GET",
     dataType: "json",
     data: $form.serializeObject(),
-    success:function(data) {
-    console.log('success');
-    // window.location.href = 'nomination?nomination_name=' + data.nomination_name + "&nomination_reason=" + data.nomination_reason + "&submitter_name=" + data.submitter_name;
-  }
+    success: function() {
+      // Success message
+      $('#success').html("<div class='alert alert-success'>");
+      $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+      $('#success > .alert-success')
+        .append("<strong>Your message has been sent. </strong>");
+      $('#success > .alert-success')
+        .append('</div>');
+      //clear all fields
+      $('#contactForm').trigger("reset");
+    },
+    error: function() {
+      // Fail message
+      $('#success').html("<div class='alert alert-danger'>");
+      $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+        .append("</button>");
+      $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+      $('#success > .alert-danger').append('</div>');
+      //clear all fields
+      $('#contactForm').trigger("reset");
+    },
   });
 })
